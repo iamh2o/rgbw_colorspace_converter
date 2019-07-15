e23dfeb751483a780dfb12842b345a9e24ae49a8
John Major <john.e.major.jr@gmail.com>
2019-07-15 02:48:02 -0400

    remove unuseful helper scripts

--- scripts/emit_led_strip_geom.py
+++
@@ -1,8 +0,0 @@
-dmx_start = 1
-device = 1
-top_neighbor = 0
-print("\t".join(["Device", "TopNeighbor", "BottomNeighbor"]))
-while device <= 255:
-    print("%s\t%s\t%s" % (device, top_neighbor, device+1))
-    top_neighbor += 1
-    device += 1
--- scripts/emit_led_strip_json.py
+++
@@ -1,8 +0,0 @@
-dmx_start = 1
-device = 1
-print('{"1":{')
-while dmx_start <= 512:
-    print('"%s": %s,' % (device, dmx_start))
-    dmx_start = dmx_start+4
-    device = device+1
-print("}}")
