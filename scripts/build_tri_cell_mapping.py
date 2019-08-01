5737ee20d0273bdeaa701db68def3fa8cb30c644
John Major <john.e.major.jr@gmail.com>
2019-08-01 00:44:49 -0700

    fixed f'd up first attemt at mapping

--- scripts/build_tri_cell_mapping.py
+++ scripts/build_tri_cell_mapping.py
@@ -2,50 +2,37 @@ import os
 import sys
 
 curr_cell = 0
-end_led = 1059
-n = end_led
 
-ds = {1:1,
-      2:3,
-      3:5,
-      4:7,
-      5:9,
-      6:11,
-      7:13,
-      8:15,
-      9:17,
-      10:19,
-      11:21
+#Built from https://docs.google.com/spreadsheets/d/16Ys242V437N968W5UU2WQdonFJ6SwZJeYmEXGbC9cYo/edit#gid=0
+ds = {1: [1, 1051,0],
+      2: [3, 1018,1050],
+      3: [5, 969, 1017],
+      4: [7, 904, 968],
+      5: [9, 823, 903],
+      6: [11, 726,822],
+      7: [13, 613, 725],
+      8: [15, 484,612],
+      9: [17, 339, 483],
+      10: [19, 178,338],
+      11: [21, 1, 177]
       }
-u = end_led -7
-d = end_led
 
-for i in range (1,12):
-    n_cells = ds[i]
+for row in ds:
+    n_cells = ds[row][0] # ncells in row
+    u = ds[row][1] # up cell counter
+    d = ds[row][2] # down cell counter
 
     is_up = True
-    tot_u_add = 0
-    tot_d_add = 0
     for cell in range (1, n_cells+1):
         if is_up:
             print('{0}: [{1},{2},{3},{4},{5},{6},{7},{8}],'.format(curr_cell, u, u+1, u+2 , u+3, u+4, u+5, u+6, u+7))
             is_up = False
             u += 8
-            tot_u_add += 8
+     
         else:
             print('{0}: [{1},{2},{3},{4},{5},{6},{7},{8}],'.format(curr_cell, d ,d+1, d+2, d+3, d+4,d+5,d+6, d+7))
             d += 8
-            tot_d_add +=8
             is_up = True
 
         curr_cell += 1
 
-    md = 0
-    if i == 1:
-        md = (i*8)+tot_d_add
-    else:
-        md = 9+((n_cells)*8)+tot_d_add
-    d -= md
-
-    mu = 9+((n_cells+2)*8)+tot_u_add
-    u -= mu
