d6f97a6176e0a597ba69e5643e3bc5ed366f5e66
John Major <john.e.major.jr@gmail.com>
2019-08-02 01:33:54 -0700

    New pixel mapping assuming 4 full strands per triangle

--- scripts/build_pixel_map_json.py
+++ scripts/build_pixel_map_json.py
@@ -8,9 +8,15 @@ pixel_num = 1
 print("{")
 for ux in range(1, num_ux+1):
     dmx_start = 1
-    while dmx_start < 512:
-        print('"{0}": [{1}, {2}],'.format(pixel_num,ux,dmx_start))
-        pixel_num += 1
-        dmx_start += 4
+    if ux in (3,6,9,12):
+        while dmx_start < 176:
+            print('"{0}": [{1}, {2}],'.format(pixel_num,ux,dmx_start))
+            pixel_num += 1
+            dmx_start += 4
+    else:
+        while dmx_start < 512:
+            print('"{0}": [{1}, {2}],'.format(pixel_num,ux,dmx_start))
+            pixel_num += 1
+            dmx_start += 4
 
 print("}")
