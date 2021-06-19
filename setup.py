3c75ef1abbf79791ae460d99220818b501a2f5d9
John Major <john.major@invitae.com>
2021-06-19 05:47:32 +0000

    more install stuff

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rgbw_colorspace_converter",
+    version="0.0.1",
+    author="John Major",
+    author_email="iamh2o@gmail.com",
+    description="Convert between RGB / HSV / HSL / HSI / HEX Color Spaces. And, emit the RGBW code for each.",
+    long_description="see description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamh2o/rgbw_colorspace_converter",
+    project_urls={
+        "Bug Tracker": "https://github.com/iamh2o/rgbw_colorspace_converter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
