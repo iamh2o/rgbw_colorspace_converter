3c46de7905931e67c4f5c9c57871bace55f11a8b
Daniel Miller <Peaches491@gmail.com>
2021-07-07 22:59:36 +0100

    Fix bad import in colors.util pkg

---
+++ tests/test_color_transition.py
@@ -0,0 +1,15 @@
+from rgbw_colorspace_converter.colors.converters import RGB
+from rgbw_colorspace_converter.colors.util.morph import color_transition
+
+def test_color_transition():
+    start = RGB(255, 0, 0)
+    end = RGB(0, 255, 0)
+    steps = 25
+
+    count = 0
+    previous = start
+    for color in color_transition(start, end, steps=steps):
+        count += 1
+        assert color.rgb_r <= previous.rgb_r
+        previous = color
+    assert count == steps
