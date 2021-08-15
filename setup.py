3dc92e60ef72943ed9f279692fdcb9ffe4eb37cb
iamh2o <iamh2o@gmail.com>
2021-08-14 17:46:16 -0700

    tagget v0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rgbw_colorspace_converter",
-    version="0.0.9",
+    version="v0.0.9",
     author="John Major",
     author_email="iamh2o@gmail.com",
     description="Convert between RGB / HSV / HSL / HSI / HEX Color Spaces. And, emit the RGBW code for each.",
