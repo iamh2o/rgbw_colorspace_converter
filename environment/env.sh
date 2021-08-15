7bcf7870902511f6c8c8e7121dfe85d5d0f561dc
iamh2o <iamh2o@gmail.com>
2021-08-14 17:24:30 -0700

    clean up install scripts

--- environment/env.sh
+++ environment/env.sh
@@ -1,7 +1,7 @@
-#!/bin/bash                                                                     
+#!/bin/bash
 
-# Nonsense to get conda install location                                    
-A=`which conda`
-B=`dirname $A`/
+# Nonsense to get conda install location
+A=$(which conda)
+B=$(dirname $A)/
 export CONDA_DIR=$B..
 source $CONDA_DIR/etc/profile.d/conda.sh
--- environment/setup.sh
+++ environment/setup.sh
@@ -6,28 +6,28 @@
 
 export repo=HBP
 
-if [[ "$1" != "$repo" ]]; then
+if [[ $1 != "$repo" ]]; then
 
-    echo """
+	echo """
 
     Hello! This is the  __$repo___installer. This script will have almost certainly have died the time you read this.
 
     """
-    echo "
+	echo "
     If you ran this script to see what it would do, you're in luck, it says hello and has not installed a conda env for you :-)  (yet!)."
-    echo "
+	echo "
     	  If you simply wish to use the library, put  ./lib in your PYTHONPATH and import color.   See the comments in ./lib/color.py for more info.
 
           And if you wish it to create a conda env, with a SUPER minimal color test UI... continue reading please.
 
 	  "
-    echo "
+	echo "
 	   To install $repo, you'll enter 2 arguments: '$repo' CONDA_DIR(/path/to/conda).
 
            If CONDA_DIR is an existing conda root directory, the conda install will be skipped and the supersonic annd mantasvenv2 envs will be created using the existing conda install--- though, mamaba will be installed into the base conda env(you can skip this part, but are now trailblazing and all waranties are void) ( https://github.com/mamba-org/mamba). "
-    echo "
+	echo "
                mamba installs to base. Then the $repo yaml file is used to build the env. It should be fast if you're used to old skool conda.  You'll need the base environment to be called 'base' for this to (hopefully) run.  You are free to hack though."
-    echo "
+	echo "
 																	Right.
 														   To recap: to install $repo, run this script again with the first argument being '$repo' and the second argument being the path to install conda to -OR- your existing conda install dir. If there is a file or dir in the place specified, conda base environment install will be skipped and the existing codna env will be attempted to be used.  The third argument is if you would like to run the post installation micro-test, type yes to run the tests and no to skip them (not implemented yet).
 
@@ -35,19 +35,19 @@ if [[ "$1" != "$repo" ]]; then
 ie: ./setup.sh $repo $HOME/conda [no|yes]
 
 "
-    exit 15
+	exit 15
 fi
 
 echo "$repo----------------------------PrepareForLaunch"
 sleep 2
 
-if [[ "$2" == "" ]]; then
-    echo "
+if [[ $2 == "" ]]; then
+	echo "
 
     .......Please specify a path to find/install conda as argument #2
 
     "
-    exit 33
+	exit 33
 fi
 
 export CONDA_DIR=$2
@@ -59,9 +59,9 @@ export CPUS=$(fgrep processor /proc/cpuinfo | wc -l)
 
 # Install miniconda if you don't have it
 if [[ ! -d $CONDA_DIR ]]; then
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_DIR
-    rm Miniconda3-latest-Linux-x86_64.sh
+	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_DIR
+	rm Miniconda3-latest-Linux-x86_64.sh
 fi
 
 source $CONDA_DIR/etc/profile.d/conda.sh
@@ -74,7 +74,7 @@ conda install -y -n base -c conda-forge mamba
 
 # Create conda environment
 echo "CREATING $e"
-mamba env create  -n $repo -f $repo.yaml
+mamba env create -n $repo -f $repo.yaml
 
 conda activate $repo
 
@@ -98,4 +98,4 @@ echo "
 "
 
 bin/run_spectrum_saturation_cycler.py 2
-exit 0;
+exit 0
