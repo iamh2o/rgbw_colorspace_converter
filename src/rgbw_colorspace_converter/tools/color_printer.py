91241adaf0f3cb2ae15d318b408e3d915bd4b2fb
iamh2o <iamh2o@gmail.com>
2021-08-16 01:50:52 -0700

    working up to interesting bits

--- src/rgbw_colorspace_converter/tools/color_printer.py
+++ src/rgbw_colorspace_converter/tools/color_printer.py
@@ -8,8 +8,8 @@ def print_colors(
     print_chars="_",
     ansi_bat_f=None,
     ansi_html_f=None,
-    col_width=78,
-    print_codes="no",
+    col_width=10,
+    print_color_codes=False,
     random_col_len=False,
     no_newlines=False,
     right_just_term_width="",
@@ -19,9 +19,15 @@ def print_colors(
     random_block_len=False,
 ):
 
-    if check_term_size:
-        col_width = os.get_terminal_size().columns - 2
+    no_newlines_flag = ""
+    if no_newlines:
+        no_newlines_flag = " -n "
 
+    print(check_term_size, "  PPP: ", col_width)
+    if check_term_size:
+        col_width = os.get_terminal_size().columns
+        print("III", col_width)
+    print("AAAA", col_width)
     ret_code = None
     bgcolor = "111111"
     if print_bars is True:
@@ -32,15 +38,15 @@ def print_colors(
         cap_o = f" | tee -a {ansi_bat_f} "
 
     l = ""
-    if print_codes == "no":
+    if print_color_codes is False:
         # just print colors
         r = 1
         if random_block_len:
             r = random.randint(1, col_width)
 
-        len_pcs = len(print_chars)
-        l = print_chars * (int(col_width / len_pcs) * r)
-        cmd = f"""colr {right_just_term_width}  {no_newlines} " {l} " "{color.hex}" "{bgcolor}"  {cap_o} 2>/dev/null;"""
+        cw = int(col_width * r) - 1
+        l = str(print_chars * cw)[0:cw]
+        cmd = f"""colr {right_just_term_width}  {no_newlines_flag} " {l} " "{color.hex}" "{bgcolor}"  {cap_o} 2>/dev/null;"""
         ret_code = os.system(cmd)
     else:
         # Prtint color codes with color blocks
@@ -48,4 +54,6 @@ def print_colors(
         cmd = f"""colr  " {l} " "000000" "{color.hex}" {cap_o} 2>/dev/null;"""
         ret_code = os.system(cmd)
 
+    # from IPython import embed
+    # embed()
     return (int(ret_code), int(col_width))
