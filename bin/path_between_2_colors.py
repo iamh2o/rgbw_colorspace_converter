636952d5580c9912df05ea3c6cecc2769b7edf34
iamh2o <iamh2o@gmail.com>
2021-08-16 04:28:45 -0700

    active third script

--- bin/path_between_2_colors.py
+++ bin/path_between_2_colors.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+
 from rgbw_colorspace_converter.colors.converters import RGB, HSV
 from rgbw_colorspace_converter.tools.color_printer import print_colors
 import rgbw_colorspace_converter.colors.util.morph as clrmorph
 
-red_color_obj = RGB(255, 255, 0)
+# Initializing a YELLOW color object and a BLUE color object.
+yellow_color_obj = RGB(255, 255, 0)
 blue_color_obj = RGB(0, 0, 255)
-path_between = clrmorph.color_transition(red_color_obj, blue_color_obj, steps=12)
+# Using a utility function to find a path betwwn the 2 colors
+path_between = clrmorph.color_transition(yellow_color_obj, blue_color_obj, steps=12)
+
+print('Note how clear the HSV system changes are, only requiring one value to change, where RGB is pretty counterintuitive in the transition')
 for color_step in path_between:
     print_colors(color_step, check_term_size=False)
     print(
